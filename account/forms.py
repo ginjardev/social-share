ff6a9424f765841958daa0586fa2dd4c5a152dc6
ginjardev <arthurshur@gmail.com>
2023-07-12 14:50:09 +0000

    email authentication

--- account/forms.py
+++ account/forms.py
@@ -19,12 +19,25 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        queryset = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if queryset.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
