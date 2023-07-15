b175ded2895872c9532357b08a1c68a3493b9913
ginjardev <arthurshur@gmail.com>
2023-07-15 23:11:06 +0000

    images model"

--- images/models.py
+++ images/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Image(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             related_name='images created', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d')
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+        order = ['-created']
+
+    def __str__(self):
+        return self.title
+
