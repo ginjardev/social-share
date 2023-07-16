5f115a3aba7a2dfbc4f86d2679a793011f6403f4
ginjardev <arthurshur@gmail.com>
2023-07-16 22:25:34 +0000

    image save

--- images/forms.py
+++ images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -9,3 +12,23 @@ class ImageCreateForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpeg', 'jpg', 'png']
+        extension = url.rsplit('.')[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
+    
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
