a6e261d605e2c303e021840e334c5a511a242e84
ginjardev <arthurshur@gmail.com>
2023-07-17 22:43:37 +0000

    views modified

--- images/views.py
+++ images/views.py
@@ -11,6 +11,7 @@ def image_create(request):
         if form.is_valid():
             cd = form.cleaned_data
             new_image = form.save(commit=False)
+            new_image.user = request.user
             new_image.save()
             messages.success(request='Image added successfully')
             return redirect(new_image.get_absolute_url())
