f665d0ce5d93429ad99d0a47359a1ffef89d91a2
ginjardev <arthurshur@gmail.com>
2023-07-20 23:02:31 +0000

    loading images

--- images/forms.py
+++ images/forms.py
@@ -16,7 +16,7 @@ class ImageCreateForm(forms.ModelForm):
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpeg', 'jpg', 'png']
-        extension = url.rsplit('.')[1].lower()
+        extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
