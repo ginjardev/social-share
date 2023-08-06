39ed6553dbeeacd8e02cfa27581271fca320209f
ginjardev <arthurshur@gmail.com>
2023-08-06 20:51:04 +0000

    url patterns

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from .views import user_login
 from django.contrib.auth import views as auth_views
-from .views import dashboard, register, edit, user_list, user_detail
-
+from . import views 
 
 urlpatterns = [
     # path('login/', user_login, name='login'),
@@ -15,9 +14,10 @@ urlpatterns = [
     # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
-    path('', dashboard, name='dashboard'),
-    path('register/', register, name='register'),
-    path('edit/', edit, name='edit'),
-    path('users/', user_list, name='user_list'),
-    path('users/<username>/', user_detail, name='user_detail'),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
 ]
