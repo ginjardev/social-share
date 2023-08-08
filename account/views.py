9c0b6060448e49bbd9bd4ff51eb5a15fac5cf9ea
ginjardev <arthurshur@gmail.com>
2023-08-08 21:44:32 +0000

    user actions

--- account/views.py
+++ account/views.py
@@ -11,6 +11,7 @@ from .models import Profile
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from .models import Contact
+from actions.utils import create_action
 
 
 def home(request):
@@ -49,6 +50,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
@@ -93,6 +95,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from = request.user, user_to = user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to = user).delete()
             return JsonResponse({'status':'ok'})
