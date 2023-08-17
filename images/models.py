ca9b01431e1e661a9770e8aadc838182e1ae7568
ginjardev <arthurshur@gmail.com>
2023-08-17 23:56:03 +0000

    signals

--- images/models.py
+++ images/models.py
@@ -10,6 +10,7 @@ class Image(models.Model):
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d')
+    total_likes = models.PositiveIntegerField(default=0)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
@@ -17,7 +18,8 @@ class Image(models.Model):
 
     class Meta:
         indexes = [
-            models.Index(fields=['-created'])
+            models.Index(fields=['-created']),
+            models.Index(fields=['-total_likes'])
         ]
         ordering = ['-created']
 
