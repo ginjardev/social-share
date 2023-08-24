be88563afd05018a23737451183a1f797acf43c2
ginjardev <arthurshur@gmail.com>
2023-08-24 02:43:56 +0000

    redis image count check

--- images/views.py
+++ images/views.py
@@ -12,7 +12,7 @@ from actions.utils import create_action
 import redis 
 from django.conf import settings
 
-r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_DB, db=settings.REDIS_DB)
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
