c3a178023cbcb0a1390a14fe95a6eb020fb31303
ginjardev <arthurshur@gmail.com>
2023-11-16 15:23:30 +0000

    requirements

--- images/views.py
+++ images/views.py
@@ -35,13 +35,15 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = r.incr(f'image:{image.id}:views')
-    r.zincrby('image_rank', 1, image.id)
+    w = r.zincrby('image_rank', 1, image.id)
+    print(w)
     return render(request, 'images/image/detail.html', 
                   {'section':'images', 'image': image, 'total_views':total_views})
 
 @login_required
 def image_ranking(request):
-    image_ranking = r.zrange('image_ranking', 0, 1, desc=True)[:10]
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    print(image_ranking)
     image_ranking_ids = [int(id) for id in image_ranking]
     most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
